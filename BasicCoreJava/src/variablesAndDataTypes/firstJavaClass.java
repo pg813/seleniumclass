4c5f7dca19bf87c94e36dce6a14b99096368fe91
poojag <poojagilbile813@gmail.com>
2020-01-25 12:47:59 +0530

    committing first java class

---
+++ BasicCoreJava/src/variablesAndDataTypes/firstJavaClass.java
@@ -0,0 +1,13 @@
+package variablesAndDataTypes;
+
+public class firstJavaClass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("welcome to automation classes");
+		
+		
+
+	}
+
+}
